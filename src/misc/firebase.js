29966e92f28de9949634d2f64817e8709384de43
chetan <awasthichetan0@gmail.com>
2022-08-22 13:56:10 +0530

    added sign in page with google/facebook

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from "firebase/app";
+import 'firebase/auth' ;
+import 'firebase/database' ;
 
 
 const config = {
@@ -13,4 +15,5 @@ const config = {
 
 
 const app = firebase.initializeApp(config);
-
+export const auth = app.auth();
+export const database = app.database();
