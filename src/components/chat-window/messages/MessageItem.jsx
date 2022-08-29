35f8ad87f8560985fc547aacbd4d3a0fb53b5505
chetan <awasthichetan0@gmail.com>
2022-08-29 19:24:46 +0530

    added button to grant or revoke admin access

--- src/components/chat-window/messages/MessageItem.jsx
+++ src/components/chat-window/messages/MessageItem.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { memo } from 'react';
+import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { auth } from '../../../misc/firebase';
 import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
-function MessageItem({ message }) {
+function MessageItem({ message,handleAdmin }) {
   const { author, createdAt, text } = message;
+
+  const isAdmin = useCurrentRoom(v => v.isAdmin);
+  const admins = useCurrentRoom(v => v.admins);
+
+  const isMsgAuthorAdmin = admins.includes(author.uid);
+  const isAuthor = auth.currentUser.uid === author.uid;
+  const canGrantAdmin = isAdmin && !isAuthor;
 
   return (
     <li className="padded mb-1">
       <div className="d-flex align-items-center font-bolder mb-1">
-
-   <PresenceDot uid={author.uid} />
+        <PresenceDot uid={author.uid} />
 
         <ProfileAvatar
           src={author.avatar}
@@ -19,8 +28,24 @@ function MessageItem({ message }) {
           className="ml-1"
           size="xs"
         />
-        <ProfileInfoBtnModal profile={author} appearance="link" className="p-0 ml-1 text-black" />
-        <TimeAgo datetime={createdAt} className="font-normal text-black-45 ml-2" />
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="p-0 ml-1 text-black"
+        >
+          {canGrantAdmin && (
+            <Button block onClick={() => handleAdmin(author.uid)} color="blue">
+              {isMsgAuthorAdmin
+                ? 'Remove admin permission'
+                : 'Give admin in this room'}
+            </Button>
+          )}
+        </ProfileInfoBtnModal>
+
+        <TimeAgo
+          datetime={createdAt}
+          className="font-normal text-black-45 ml-2"
+        />
       </div>
 
       <div>
@@ -30,4 +55,4 @@ function MessageItem({ message }) {
   );
 }
 
-export default MessageItem;
+export default memo(MessageItem);
--- src/components/chat-window/messages/ProfileInfoBtnModal.jsx
+++ src/components/chat-window/messages/ProfileInfoBtnModal.jsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import { Button, Modal } from 'rsuite'
-import {useModelState} from '../../../misc/custom-hooks'
+import React from 'react';
+import { Button, Modal } from 'rsuite';
+import { useModelState } from '../../../misc/custom-hooks';
 import ProfileAvatar from '../../ProfileAvatar';
 
-function ProfileInfoBtnModal({profile, ...btnProps}) {
+function ProfileInfoBtnModal({ profile,children, ...btnProps }) {
+  const { isOpen, close, open } = useModelState();
+  const { name, avatar, createdAt } = profile;
 
-    const {isOpen,close,open} = useModelState();
-    const {name,avatar,createdAt} = profile;
+  const shortName = profile.name.split(' ')[0];
 
-const shortName = profile.name.split(' ')[0];
-
-const memberSince = new Date(createdAt).toLocaleDateString();
+  const memberSince = new Date(createdAt).toLocaleDateString();
 
   return (
     <>
-        <Button  {...btnProps} onClick={open}>
-            {shortName}
-        </Button>
-        <Modal show={isOpen} onHide={close}>
+      <Button {...btnProps} onClick={open}>
+        {shortName}
+      </Button>
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>{shortName} profile</Modal.Title>
+        </Modal.Header>
 
-      <Modal.Header>
-        <Modal.Title>
-        {shortName} profile
-        </Modal.Title>
-      </Modal.Header>
+        <Modal.Body className="text-center">
+          <ProfileAvatar
+            src={avatar}
+            name={name}
+            className="width-200 height-200 img-fullsize font-huge"
+          />
+          <h4 className="mt-2">{name}</h4>
+          <p>Member since {memberSince}</p>
+        </Modal.Body>
 
-      <Modal.Body className='text-center'>
-      <ProfileAvatar src={avatar} name = {name} className="width-200 height-200 img-fullsize font-huge" />
-      <h4 className='mt-2'>{name}</h4>
-      <p>Member since {memberSince}</p>
-      </Modal.Body>
-
-      <Modal.Footer>
-   <Button block onClick={close}>
-  Close
-   </Button>
-      </Modal.Footer>
-
-        </Modal>
+        <Modal.Footer>
+          {children}
+          <Button block onClick={close}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
-  )
+  );
 }
 
-export default ProfileInfoBtnModal
+export default ProfileInfoBtnModal;
